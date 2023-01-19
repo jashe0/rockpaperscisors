aa44a185a80eee5af82c171f655f1442b8d4005d
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-01-19 19:51:04 +0000

    add index.html styles.css script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    
+    
+    function playRound(playerSelection, computerSelection) {
+        
+    }
+    
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+</script>
