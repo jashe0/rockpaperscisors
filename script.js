fb38c74835ffa368aa74d786e050cc4064c7b285
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-01-22 18:04:28 +0000

    moved script to script.js add player selection prompt function

---
+++ script.js
@@ -0,0 +1,69 @@
+    const computerOptions = ["rock", "paper", "scissors"]
+                
+
+    function getComputerChoice() {
+        const computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+        return computerChoice
+    }    
+        
+    function checkWinner (playerSelection, computerSelection){
+        if (playerSelection == computerSelection){
+            return "Tie"
+        }
+        else if(
+            (playerSelection == "rock" && computerSelection == "scissors") ||
+            (playerSelection == "scissors" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "rock")
+        ){
+            return "Player"
+        } else {
+            return "Computer"
+        }
+    }
+
+    function playRound(playerSelection,computerSelection) {                
+        const result = checkWinner(playerSelection,computerSelection)
+        if (result == "Tie"){
+            return "It's a tie!"
+        } else if (result == "Player"){
+            return "You win! " + (playerSelection) + " beats " +(computerSelection)
+        } else {
+            return "You lose! " + (computerSelection) + " beat " +(playerSelection)
+        }
+                
+            
+    }
+
+    function getPlayerChoice() {
+        let validInput = false
+        while (validInput == false){
+                const playerChoice = prompt("Rock, paper or scissors?")
+                        if (playerChoice == null){
+                            continue
+                        } 
+                        const choiceInLower = playerChoice.toLowerCase()
+                        if (computerOptions.includes(choiceInLower)){
+                            validInput = true
+                            return choiceInLower
+                        }
+        }
+    }
+
+    const playerSelection = getPlayerChoice()
+    const computerSelection =  getComputerChoice()
+    console.log(playRound(playerSelection,computerSelection))
+
+
+    /**
+
+    function game() {
+                console.log('Welcome!')
+                for (let i = 0; i < 5; i++) {
+                const playerSelection = getPlayerChoice()  
+                const computerSelection =  getComputerChoice()
+                console.log(playRound(playerSelection,computerSelection))
+            }                   
+            }
+    
+    game()    **/    
+            
