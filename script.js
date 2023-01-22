8262f7cfa53809dd8a2890492b5e0f6b9ccdaf28
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-01-22 19:21:24 +0000

    add overall win lose function clean up some code

--- script.js
+++ script.js
@@ -50,12 +50,27 @@
     } 
 
     function game() {
-                console.log('Welcome!')
+                let playerScore = 0;
+                let computerScore = 0;
+                console.log("Welcome!")
                 for (let i = 0; i < 5; i++) {
                 const playerSelection = getPlayerChoice();
                 const computerSelection =  getComputerChoice();
                 console.log(playRound(playerSelection,computerSelection));
-            }                   
+                if (checkWinner(playerSelection,computerSelection)=="Player"){
+                    playerScore ++;
+                } else if (checkWinner(playerSelection, computerSelection)=="Computer") {
+                    computerScore++;
+                }
+                }    
+                console.log("Game over")
+                if(playerScore > computerScore){
+                    console.log("Player won the game!");
+                }else if (playerScore < computerScore){
+                    console.log("Computer won the game!");
+                }else{
+                    console.log("It was a tie!"); 
+                }             
             }
     
     game()      
